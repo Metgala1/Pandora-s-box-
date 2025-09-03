95c0d7d8cb0058b7aa5bd7a2d5b6e9fbcdad8a1e
ATAGWE ROGER <atagweroger2@gmail.com>
2025-09-03 17:40:56 +0100

    Worked on the navgation bar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <div id="app">
+       <Navbar />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+    const [user, setUser] = useState(true)
+
+  return (
+    <nav className={styles.navBar}>
+          <a className={styles.brand} href="/">
+            <img
+              src="logo.png"
+              alt="Pandora’s Box Logo"
+              style={{ width: "60px", height: "60px", borderRadius: "10px" }}
+            />
+            <p className={styles.gradientText}>Pandora’s Box</p>
+          </a>
+
+      <div className={styles.controll}>
+        {user ? (
+          <button onClick={() => setUser(!user)} className={`${styles.logoutBtn} ${styles.btn}`}>Logout</button>
+        ) : (
+          <div className={styles.loginRegDiv}>
+            <button className={`${styles.loginBtn} ${styles.btn}`}>Login</button>
+            <button className={`${styles.registerBtn} ${styles.btn}`}>Register</button>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
